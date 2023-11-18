cfa3d05fefec6a429eea9f4aaeb1173004f155de
Mert Kavilcioğlu <mertkavilci@gmail.com>
2023-11-18 16:45:18 +0300

    Added c1,c2,p1,p2 musics

--- Assets/Scripts/AudioManager.cs
+++ Assets/Scripts/AudioManager.cs
@@ -8,6 +8,8 @@ public class AudioManager : MonoBehaviour
 
     public AudioClip c1;
     public AudioClip p1;
+    public AudioClip c2;
+    public AudioClip p2;
 
     private void Start()
     {
@@ -18,7 +20,24 @@ public class AudioManager : MonoBehaviour
     public void StartP1()
     {
         musicSource.Stop();
+        musicSource.volume = 0.02f;
         musicSource.clip = p1;
+        musicSource.Play();
+    }
+
+    public void StartC2()
+    {
+        musicSource.Stop();
+        musicSource.volume = 0.07f;
+        musicSource.clip = c2;
+        musicSource.Play();
+    }
+
+    public void StartP2()
+    {
+        musicSource.Stop();
+        musicSource.volume = 0.02f;
+        musicSource.clip = p2;
         musicSource.Play();
     }
 }
